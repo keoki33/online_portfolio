e11b56b6b4dfb2deb313b75a36de724a0a2f26cf
George <gwong323@yahoo.com>
2019-09-03 22:47:59 +0100

    state, functions, home done, need components

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,49 @@ import Footer from "./Footer";
 import Html from "./icons/Html";
 import Css from "./icons/Css";
 import Scss from "./icons/Scss";
+import RubyLogo from "./icons/RubyLogo";
+import SinatraLogo from "./icons/SinatraLogo";
+import Rails from "./icons/Rails";
+import Action from "./icons/Action";
+import Javascript from "./icons/Javascript";
+import ReactLogo from "./icons/ReacLogo";
+import Sql from "./icons/Sql";
+import GoogleLogo from "./icons/GoogleLogo";
+import Heroku from "./icons/Heroku";
+import Netlify from "./icons/Netlify";
+import Comptia from "./icons/Comptia";
+import Zendesk from "./icons/Zendesk";
+import Photoshop from "./icons/Photoshop";
+import Ase from "./icons/Ase";
+import Stay from "./icons/Stay";
 
 import { ReactComponent as Ruby } from "./images/skills/ruby.svg";
 import { ReactComponent as Sinatra } from "./images/skills/sinatra.svg";
 import { ReactComponent as Google } from "./images/skills/google.svg";
 
 class Home extends Component {
-  state = { class: "home", html: true, css: true, scss: true, skills: "" };
+  state = {
+    class: "home",
+    html: true,
+    css: true,
+    scss: true,
+    ruby: true,
+    sinatra: true,
+    rails: true,
+    action: true,
+    javascript: true,
+    react: true,
+    sql: true,
+    google: true,
+    heroku: true,
+    netlify: true,
+    comptia: true,
+    zendesk: true,
+    photoshop: true,
+    ase: true,
+    stay: true,
+    skills: ""
+  };
 
   whatever = () => {};
 
@@ -24,7 +60,26 @@ class Home extends Component {
   };
 
   skillsActive = () => {
-    if (this.state.html && this.state.css && this.state.scss) {
+    if (
+      this.state.html &&
+      this.state.css &&
+      this.state.scss &&
+      this.state.ruby &&
+      this.state.sinatra &&
+      this.state.rails &&
+      this.state.action &&
+      this.state.javascript &&
+      this.state.react &&
+      this.state.sql &&
+      this.state.google &&
+      this.state.heroku &&
+      this.state.netlify &&
+      this.state.comptia &&
+      this.state.zendesk &&
+      this.state.photoshop &&
+      this.state.ase &&
+      this.state.stay
+    ) {
       this.setState({ skills: "" });
     } else {
       this.setState({ skills: "skillsActive" });
@@ -32,8 +87,28 @@ class Home extends Component {
   };
 
   handleSkills = () => {
-    this.setState({ html: true, css: true, scss: true }, () =>
-      this.skillsActive()
+    this.setState(
+      {
+        html: true,
+        css: true,
+        scss: true,
+        ruby: true,
+        sinatra: true,
+        rails: true,
+        action: true,
+        javascript: true,
+        react: true,
+        sql: true,
+        google: true,
+        heroku: true,
+        netlify: true,
+        comptia: true,
+        zendesk: true,
+        photoshop: true,
+        ase: true,
+        stay: true
+      },
+      () => this.skillsActive()
     );
   };
 
@@ -71,13 +146,62 @@ class Home extends Component {
           {this.state.html ? <Html implode={this.implode} /> : console.log()}
           {this.state.css ? <Css implode={this.implode} /> : console.log()}
           {this.state.scss ? <Scss implode={this.implode} /> : console.log()}
+          {this.state.ruby ? <Ruby implode={this.implode} /> : console.log()}
+          {this.state.sinatra ? (
+            <SinatraLogo implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.rails ? <Rails implode={this.implode} /> : console.log()}
+          {this.state.action ? (
+            <Action implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.javascript ? (
+            <Javascript implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.react ? (
+            <ReactLogo implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.sql ? <Sql implode={this.implode} /> : console.log()}
+          {this.state.google ? (
+            <GoogleLogo implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.heroku ? (
+            <Heroku implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.netlify ? (
+            <Netlify implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.comptia ? (
+            <Comptia implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.zendesk ? (
+            <Zendesk implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.photoshop ? (
+            <Photoshop implode={this.implode} />
+          ) : (
+            console.log()
+          )}
+          {this.state.ase ? <Ase implode={this.implode} /> : console.log()}
+          {this.state.stay ? <Stay implode={this.implode} /> : console.log()}
           <p>
-            {/* <img
-              className="skillsLogo"
-              src={require("./images/skills/ruby.jpeg")}
-              alt=""
-            /> */}
-            {/* <Ruby className="rubyLogo" /> */}
             <svg
               className="rubyLogo"
               xmlns="http://www.w3.org/2000/svg"
